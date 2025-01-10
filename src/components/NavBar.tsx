c3dffa737bad93381be32551ad504ae8fcf2d13a
Ates Yigit Onel <ates.onel@gmail.com>
2025-01-10 23:33:47 +0300

    navbar değişiklikler yapıldı

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import "./NavBar.css";
+import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
+import EmailIcon from "@mui/icons-material/Email";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import { Menu, MenuItem, Button } from "@mui/material";
+
 const Navbar: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+
+  const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <nav className="navbar">
       <div className="logo">Finger Chat</div>
@@ -10,14 +26,47 @@ const Navbar: React.FC = () => {
         <li>Promotions</li>
         <li>Club Elite</li>
         <li>Top Members</li>
+        <li>
+          <EmojiEventsIcon style={{ color: "white" }}></EmojiEventsIcon>
+        </li>
+        <li>
+          <EmailIcon></EmailIcon>
+        </li>
+        <li>
+          <FavoriteIcon></FavoriteIcon>
+        </li>
+        <li>
+          <button className="credits-button">Get Credits</button>
+        </li>
+        <li>
+          <select className="language-select">
+            <option value="EN">EN</option>
+            <option value="TR">TR</option>
+          </select>
+        </li>
+        <li>
+          <button
+            className="userButton"
+            style={{ color: "white" }}
+            onClick={handleMenuClick}
+          >
+            Tocoglum
+          </button>
+          <Menu
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "basic-button",
+            }}
+          >
+            <MenuItem onClick={handleClose}>Kredi Yükle</MenuItem>
+            <MenuItem onClick={handleClose}>Bilgileri Güncelle</MenuItem>
+            <MenuItem onClick={handleClose}>Arkadaşlar</MenuItem>
+            <MenuItem onClick={handleClose}>Log Out</MenuItem>
+          </Menu>
+        </li>
       </ul>
-      <div className="actions">
-        <button className="credits-button">Get Credits</button>
-        <select className="language-select">
-          <option value="EN">EN</option>
-          <option value="TR">TR</option>
-        </select>
-      </div>
     </nav>
   );
 };
